5df49dc3e2b76287b8fb20b7279df74b19b60a19
apla12 <anapaulalopes12@gmail.com>
2023-04-26 15:57:23 +0100

    add cv seccion

--- src/app/curriculo/curriculo.component.ts
+++ src/app/curriculo/curriculo.component.ts
@@ -3,13 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-curriculo',
   templateUrl: './curriculo.component.html',
-  styleUrls: ['./curriculo.component.scss']
+  styleUrls: ['./curriculo.component.scss'],
 })
 export class CurriculoComponent implements OnInit {
+  textcv: any = 'For more details, download a copy of my curriculum.';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
