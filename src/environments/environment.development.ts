38dd968803afb5b71f9b9a51eb5327ece4408e11
apla12 <anapaulalopes12@gmail.com>
2023-11-03 20:10:24 +0000

    add ambientes

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,9 +2,10 @@ export const environment = {
   firebase: {
     projectId: 'apldeveloper-cf797',
     appId: '1:1093185570159:web:2542b497c148da77e3549a',
-    databaseURL: 'https://apldeveloper-cf797-default-rtdb.europe-west1.firebasedatabase.app',
+    databaseURL:
+      'https://apldeveloper-cf797-default-rtdb.europe-west1.firebasedatabase.app',
     storageBucket: 'apldeveloper-cf797.appspot.com',
-    locationId: 'europe-west',
+    //locationId: 'europe-west',
     apiKey: 'AIzaSyClTJft5csa8U0KbNQjknUk0Q6OJ5Z408Y',
     authDomain: 'apldeveloper-cf797.firebaseapp.com',
     messagingSenderId: '1093185570159',
