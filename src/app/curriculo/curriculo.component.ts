66f6609bdf0ad5f23fa246c69bf71e191550c1a7
apla12 <anapaulalopes12@gmail.com>
2023-11-08 20:54:03 +0000

    add download file firebase

--- src/app/curriculo/curriculo.component.ts
+++ src/app/curriculo/curriculo.component.ts
@@ -1,11 +1,5 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { getDownloadURL, getStorage, ref } from 'firebase/storage';
-import { window } from 'rxjs';
-
-interface Item {
-  name: string;
-}
+import { CurriculoService } from '../service/curriculo.service';
 
 @Component({
   selector: 'app-curriculo',
@@ -15,51 +9,15 @@ interface Item {
 export class CurriculoComponent implements OnInit {
   textcv: any = 'For more details, download a copy of my curriculum.';
 
-  constructor(private _http: HttpClient) {}
+  constructor(private service: CurriculoService) {}
 
-  ngOnInit(): void {
-    console.log('init');
+  ngOnInit(): void {}
+
+  cvEN() {
+    this.service.downloadAttachment('/cv-apl-in.pdf');
   }
 
-  downloadAttachmentCvEN() {
-    console.log('EN');
-    const storage = getStorage();
-    const starsRef = ref(storage, '/cv-apl-in.pdf');
-
-    getDownloadURL(starsRef)
-      .then((url) => {
-        const xhr = new XMLHttpRequest();
-        xhr.responseType = 'blob';
-        xhr.onload = (event) => {
-          const blob = xhr.response;
-        };
-        xhr.open('GET', url);
-        xhr.send();
-      })
-      .catch((error) => {
-        // A full list of error codes is available at
-        // https://firebase.google.com/docs/storage/web/handle-errors
-        switch (error.code) {
-          case 'storage/object-not-found':
-            // File doesn't exist
-            break;
-          case 'storage/unauthorized':
-            // User doesn't have permission to access the object
-            break;
-          case 'storage/canceled':
-            // User canceled the upload
-            break;
-
-          // ...
-
-          case 'storage/unknown':
-            // Unknown error occurred, inspect the server response
-            break;
-        }
-      });
-  }
-
-  downloadAttachmentCvPT() {
-    console.log('PT');
+  cvPT() {
+    this.service.downloadAttachment('/cv-apl-pt.pdf');
   }
 }
--- src/app/service/curriculo.service.ts
+++ src/app/service/curriculo.service.ts
@@ -1,18 +1,24 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-//import { AngularFireStorage } from '@angular/fire/compat/storage';
-
+import { getDownloadURL, getStorage, ref } from 'firebase/storage';
 @Injectable({
   providedIn: 'root',
 })
 export class CurriculoService {
-  cvEN = 'gs://apldeveloper-cf797.appspot.com/cv-apl-in.pdf';
+  constructor() {}
 
-  constructor(
-
-    ) {}
-
-  public downloadAttachmentCvEN() {
-    //return this.storage.refFromURL(this.cvEN);
+  downloadAttachment(nameFile: any) {
+    const storage = getStorage();
+    getDownloadURL(ref(storage, nameFile))
+      .then(function (url) {
+        var link = document.createElement('a');
+        link.setAttribute('href', url);
+        link.style.visibility = 'hidden';
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+      })
+      .catch(function (error) {
+        console.error('Upload failed:', error);
+      });
   }
 }
