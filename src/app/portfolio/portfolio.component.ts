8f2d27a8619a600848a2751d5d48d549daac36dc
apla12 <anapaulalopes12@gmail.com>
2023-11-17 23:00:12 +0000

    add experience

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,13 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
-import { DialogbbComponent } from './dialog/dialog-bb.component';
-import { DialogCaixaComponent } from './dialog/dialog-caixa.component';
-import { DialogCamaraComponent } from './dialog/dialog-camara.component';
-import { DialogCniComponent } from './dialog/dialog-cni.component';
-import { DialogPfComponent } from './dialog/dialog-pf.component';
-import { DialogSicoobComponent } from './dialog/dialog-sicoob.component';
-import { DialogStfComponent } from './dialog/dialog-stf.component';
-import { DialogTcuComponent } from './dialog/dialog-tcu.component';
+
+class Experience {
+  position: String | undefined;
+  dateAt: String | undefined;
+  dateEnd: String | undefined;
+  client: String | undefined;
+  description: String | undefined;
+  technology: Technology | undefined;
+}
+
+class Technology {
+  backend: Backend | undefined;
+  frontend: Frontend | undefined;
+  database: Database | undefined;
+  otherInformation: String | undefined;
+}
+
+class Backend {
+  description: String | undefined;
+  timeExperience: String | undefined;
+  level: String | undefined;
+}
+
+class Frontend {
+  description: String | undefined;
+  timeExperience: String | undefined;
+  level: String | undefined;
+}
+class Database {
+  description: String | undefined;
+  timeExperience: String | undefined;
+  level: String | undefined;
+}
 
 @Component({
   selector: 'app-portfolio',
@@ -15,91 +40,44 @@ import { DialogTcuComponent } from './dialog/dialog-tcu.component';
   styleUrls: ['./portfolio.component.scss'],
 })
 export class PortfolioComponent implements OnInit {
-  experiences = [
-    { key: '1', name: 'ALL' },
-    { key: '2', name: 'POSITION' },
-    { key: '3', name: 'TECHNOLOGY' },
-    { key: '4', name: 'CLIENT' },
-    { key: '5', name: 'COMPANY' },
-  ];
-
-  classCss: any;
+  divClass: any;
 
   constructor(private dialog: MatDialog) {}
 
   ngOnInit(): void {
-    console.log(this.experiences);
+    this.filterSelection('all');
+  }
 
-    for (let item of this.experiences) {
-      if (item.key == '1') {
-        this.classCss = 'active';
-      } else {
-        this.classCss = 'filter';
+  filterSelection(name: any): void {
+    var x, i;
+    x = document.getElementsByClassName('filterDiv');
+    if (name == 'all') name = '';
+    for (i = 0; i < x.length; i++) {
+      this.w3RemoveClass(x[i], 'show');
+      if (x[i].className.indexOf(name) > -1) this.w3AddClass(x[i], 'show');
+    }
+  }
+
+  w3AddClass(element: any, name: any) {
+    let i, arr1, arr2;
+    arr1 = element.className.split(' ');
+    arr2 = name.split(' ');
+    for (i = 0; i < arr2.length; i++) {
+      if (arr1.indexOf(arr2[i]) == -1) {
+        element.className += ' ' + arr2[i];
       }
     }
   }
 
-  openDialogTCU() {
-    const dialogRef = this.dialog.open(DialogTcuComponent);
-
-    dialogRef.afterClosed().subscribe((result) => {
-      console.log(`Dialog result: ${result}`);
-    });
-  }
-
-  openDialogCNI() {
-    const dialogRef = this.dialog.open(DialogCniComponent);
-
-    dialogRef.afterClosed().subscribe((result) => {
-      console.log(`Dialog result: ${result}`);
-    });
-  }
-
-  openDialogCamara() {
-    const dialogRef = this.dialog.open(DialogCamaraComponent);
-
-    dialogRef.afterClosed().subscribe((result) => {
-      console.log(`Dialog result: ${result}`);
-    });
-  }
-
-  openDialogBB() {
-    const dialogRef = this.dialog.open(DialogbbComponent);
-
-    dialogRef.afterClosed().subscribe((result) => {
-      console.log(`Dialog result: ${result}`);
-    });
-  }
-
-  openDialogPF() {
-    const dialogRef = this.dialog.open(DialogPfComponent);
-
-    dialogRef.afterClosed().subscribe((result) => {
-      console.log(`Dialog result: ${result}`);
-    });
-  }
-
-  openDialogSicoob() {
-    const dialogRef = this.dialog.open(DialogSicoobComponent);
-
-    dialogRef.afterClosed().subscribe((result) => {
-      console.log(`Dialog result: ${result}`);
-    });
-  }
-
-  openDialogCaixa() {
-    const dialogRef = this.dialog.open(DialogCaixaComponent);
-
-    dialogRef.afterClosed().subscribe((result) => {
-      console.log(`Dialog result: ${result}`);
-    });
-  }
-
-  openDialogSTF() {
-    const dialogRef = this.dialog.open(DialogStfComponent);
-
-    dialogRef.afterClosed().subscribe((result) => {
-      console.log(`Dialog result: ${result}`);
-    });
+  w3RemoveClass(element: any, name: any) {
+    var i, arr1, arr2;
+    arr1 = element.className.split(' ');
+    arr2 = name.split(' ');
+    for (i = 0; i < arr2.length; i++) {
+      while (arr1.indexOf(arr2[i]) > -1) {
+        arr1.splice(arr1.indexOf(arr2[i]), 1);
+      }
+    }
+    element.className = arr1.join(' ');
   }
 }
